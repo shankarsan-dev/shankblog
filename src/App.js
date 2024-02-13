9d26ad1325a188ec68e1925d2dfadf5acf0f4d5f
noopali <k2sevak83@gmail.com>
2024-02-13 20:54:41 +0545

    nav bar and home

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+//import './App.css';
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
+  const title = "Welcome to my blog!"
+  const link = "https://www.shopmusical.free.nf";
   return (
+ 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+       <div ClassName = "content">
+        <Home/>
+      <h1 >{title}</h1>
+      <p>{10}</p>
+      <p>{"hello Blogger!"}</p>
+      <p>{[1,2,3,4,5]}</p>
+      <p>{Math.random(0,5)}</p>
+      <a href={link}>My shop </a>
+
+      </div>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return ( <nav className="navbar">
+        <h1>The Shank Vlog</h1>
+        <div className="links">
+            <a href="/Home">Home</a>
+            <a href="/Create">New Blog</a>
+        </div>
+        
+    </nav> );
+}
+ 
+export default Navbar;
